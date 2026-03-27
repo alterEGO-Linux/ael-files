884462269a6961a4f1d759d04d3a83b94003a632
Pascal Malouin <pascal.malouin@gmail.com>
2026-03-26 22:56:00 -0400

    ...

--- bin/busy.sh
+++ bin/busy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 # ------------------------------------------------------------------------ INFO
 # [/.ael/bin/busy]
-# author        : Pascal Malouin @https://github.com/alterEGO-Linux
+# author        : Pascal Malouin (https://github.com/alterEGO-Linux)
 # created       : 2023-08-01 19:55:12 UTC
-# updated       : 2026-01-22 06:21:00 UTC
+# updated       : 2026-03-25 10:56:01 UTC
 # description   : Look busy when the boss comes around.
 
 # --- (ref) <https://www.commandlinefu.com/commands/view/6663/pretend-to-be-busy-in-office-to-enjoy-a-cup-of-coffee>
