840b8b3191a3c992b657aebae9817d2b8f56d3c7
Pascal Malouin <pascal.malouin@gmail.com>
2026-04-02 22:42:34 -0400

    ...

--- bin/busy.sh
+++ bin/busy.sh
@@ -1,14 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # ------------------------------------------------------------------------ INFO
 # [/.ael/bin/busy]
 # author        : Pascal Malouin (https://github.com/alterEGO-Linux)
 # created       : 2023-08-01 19:55:12 UTC
-# updated       : 2026-03-25 10:56:01 UTC
+# updated       : 2026-04-02 20:41:12 UTC
 # description   : Look busy when the boss comes around.
 
-# --- (ref) <https://www.commandlinefu.com/commands/view/6663/pretend-to-be-busy-in-office-to-enjoy-a-cup-of-coffee>
-# --- CTRL+c to quit.
+busy() {
+  # --- (ref) <https://www.commandlinefu.com/commands/view/6663/pretend-to-be-busy-in-office-to-enjoy-a-cup-of-coffee>
+  # ... CTRL+c to quit.
 
-command cat /dev/urandom \
-| command hexdump -C \
-| command grep --color=always 'ca fe'
+  command cat /dev/urandom \
+  | command hexdump -C \
+  | command grep --color=always 'ca fe' | head
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    busy
+fi
